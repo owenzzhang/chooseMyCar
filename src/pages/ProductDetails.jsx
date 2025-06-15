483559cbb932de721e12b067cbffdcb72397af86
owenzzhang <owen322@hotmail.co.uk>
2025-06-15 23:31:22 +0100

    added cursor for a button

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -452,7 +452,7 @@ function ProductDetails() {
           <Card />
         </div>
         <div className="flex justify-center pb-10">
-          <button className="bg-gray-200 px-6 py-2 rounded-full">
+          <button className="bg-gray-200 px-6 py-2 rounded-full cursor-pointer">
             More Cars
           </button>
         </div>
