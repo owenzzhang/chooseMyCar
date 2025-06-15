5dcbdfec653f9ed3e9ce585a0319784b534bd893
owenzzhang <owen322@hotmail.co.uk>
2025-06-15 23:51:57 +0100

    added margin top

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -404,7 +404,7 @@ function ProductDetails() {
         </div>
       </div>
 
-      <div className="flex flex-col lg:flex-row gap-6 my-20 max-w-screen-xl mx-auto px-4">
+      <div className="flex flex-col lg:flex-row gap-6 my-20 max-w-screen-xl mx-auto px-4 mt-6">
         <img
           src={mainCar}
           alt="mainCar"
