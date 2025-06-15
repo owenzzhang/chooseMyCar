cca5e0e2c0d9d602dce720d31969e5de81d4b9e5
owenzzhang <owen322@hotmail.co.uk>
2025-06-15 23:49:19 +0100

    move rating svg to public folder

--- src/components/TrustpilotStars.jsx
+++ src/components/TrustpilotStars.jsx
@@ -1,7 +1,7 @@
 function TrustpilotStars({ stars }) {
   return (
     <img
-      src={`src/assets/ratings/${Number(stars) * 10}.svg`}
+      src={`/ratings/${Number(stars) * 10}.svg`}
       alt={`${stars}-star rating`}
       className="w-32 h-auto"
     />
